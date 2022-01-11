9256c2c574ad7b9ea9dec799d57ca44ea6809e79
ggarcia625 <57689222+ggarcia625@users.noreply.github.com>
2022-01-10 21:12:14 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Game from './Game';
+import './App.css';
+
+function App(){
+  return (
+    <div className="App">
+      <h1>Yahtzee!</h1>
+      <Game />
+    </div>
+  );
+}
+
+
+export default App;
